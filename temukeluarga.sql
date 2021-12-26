6d91c8aba1f86fa75433a8cd56b96d3b2b7c7bab
noviani-ulm <74365930+noviani-ulm@users.noreply.github.com>
2021-12-26 14:53:24 +0800

    Add files via upload

--- temukeluarga.sql
+++ temukeluarga.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Enterprise v12.5.1 (64 bit)
-MySQL - 10.4.10-MariaDB : Database - temukeluarga
+MySQL - 10.4.22-MariaDB : Database - temukeluarga
 *********************************************************************
 */
 
@@ -15,6 +15,41 @@ MySQL - 10.4.10-MariaDB : Database - temukeluarga
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`temukeluarga` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `temukeluarga`;
+
+/*Table structure for table `data` */
+
+DROP TABLE IF EXISTS `data`;
+
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(250) DEFAULT NULL,
+  `ditemukan` text DEFAULT NULL,
+  `ciri-ciri` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `data` */
+
+insert  into `data`(`id`,`nama`,`ditemukan`,`ciri-ciri`) values 
+(1,'Ana','Jl. Zafri\r\n12 November 2017\r\nMenggunakan baju hijau polos','Rambut Keriting\r\nMata Sipit'),
+(2,'Rossa',NULL,NULL),
+(3,'Zahra',NULL,NULL);
+
+/*Table structure for table `datahilangmasuk` */
+
+DROP TABLE IF EXISTS `datahilangmasuk`;
+
+CREATE TABLE `datahilangmasuk` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_hilang` varchar(250) DEFAULT NULL,
+  `nama_keluarga` varchar(250) DEFAULT NULL,
+  `kontak` varchar(250) DEFAULT NULL,
+  `foto` varchar(250) DEFAULT NULL,
+  `rincian` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `datahilangmasuk` */
 
 /*Table structure for table `failed_jobs` */
 
@@ -34,6 +69,22 @@ CREATE TABLE `failed_jobs` (
 
 /*Data for the table `failed_jobs` */
 
+/*Table structure for table `kategoriuser` */
+
+DROP TABLE IF EXISTS `kategoriuser`;
+
+CREATE TABLE `kategoriuser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `kategoriuser` */
+
+insert  into `kategoriuser`(`id`,`kategori`) values 
+(1,'admin'),
+(2,'panti');
+
 /*Table structure for table `migrations` */
 
 DROP TABLE IF EXISTS `migrations`;
@@ -52,6 +103,37 @@ insert  into `migrations`(`id`,`migration`,`batch`) values
 (2,'2014_10_12_100000_create_password_resets_table',1),
 (3,'2019_08_19_000000_create_failed_jobs_table',1),
 (4,'2019_12_14_000001_create_personal_access_tokens_table',1);
+
+/*Table structure for table `orangterdaftar` */
+
+DROP TABLE IF EXISTS `orangterdaftar`;
+
+CREATE TABLE `orangterdaftar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(250) DEFAULT NULL,
+  `foto` varchar(250) DEFAULT NULL,
+  `ditemukan` text DEFAULT NULL,
+  `rincian` text DEFAULT NULL,
+  `id_panti` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_panti` (`id_panti`),
+  CONSTRAINT `id_panti` FOREIGN KEY (`id_panti`) REFERENCES `panti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `orangterdaftar` */
+
+/*Table structure for table `panti` */
+
+DROP TABLE IF EXISTS `panti`;
+
+CREATE TABLE `panti` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(300) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `panti` */
 
 /*Table structure for table `password_resets` */
 
@@ -92,21 +174,22 @@ CREATE TABLE `personal_access_tokens` (
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `id_kategoriuser` int(11) NOT NULL,
+  `rincian` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
+  UNIQUE KEY `users_email_unique` (`email`),
+  KEY `id_kategori` (`id_kategoriuser`),
+  CONSTRAINT `id_kategori` FOREIGN KEY (`id_kategoriuser`) REFERENCES `kategoriuser` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
-
-insert  into `users`(`id`,`name`,`email`,`password`,`remember_token`,`created_at`,`updated_at`) values 
-(1,'Noviani','ukhannie@gmail.com','$2y$10$.A.95sOwnIxho0tXy5.Pyu5GRgjkM5MLpePyXrJbPyr50ssourfnC',NULL,'2021-10-10 20:52:24','2021-10-10 20:52:24');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
