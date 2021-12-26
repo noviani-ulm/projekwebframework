42a5e51f3561bf4f70a32ff7cbf4eb89c72dcf30
noviani-ulm <74365930+noviani-ulm@users.noreply.github.com>
2021-12-26 14:24:39 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,45 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view ('index', [
-        "title" => "Home"
-    ]);
+    return view('index');
+});
+
+Route::get('/form', function () {
+    return view('formhilang');
 });
 
 Route::get('/about', function () {
-    return view ('about', [
-        "title" => "About"
-    ]);
+    return view('about');
 });
 
+//Route::get('/artikel', 'App\Http\Controllers\ArticleController@index');
+Route::get('/artikel/create', 'App\Http\Controllers\ArticleController@create');
+Route::get('/artikel/{slug}', 'App\Http\Controllers\ArticleController@show');
+Route::post('/artikel', 'App\Http\Controllers\ArticleController@store');
+Route::get ('/artikel/{id}', 'App\Http\Controllers\ArticleController@edit');
+// Route::get('/data', 'App\Http\Controllers\Controller@index');
+//Route::get('/panti', 'App\Http\Controllers\Controller@panti');
 
-Route::get('/Posts', [PostController::class, 'index']);
+Route::get('/login', function () {
+    return view('login');
+});
 
+Route::get('/template', function () {
+    return view('layouts.template');
+});
 
-Route::get('/Posts/{slug}', [PostController::class, 'show']);
+Route::get('/data', function () {
+    return view('data');
+});
+
+Route::get('/panti', function () {
+    return view('panti');
+});
+
+Route::get('/artikel', function () {
+    return view('artikel.artikel');
+});
